68d946a377e7e578d0f13aeb88899b44378f5608
Gor Mkhitaryan <gor@difocus.ru>
2020-01-12 21:43:36 +0300

    Moved criteria building to trait

---
+++ src/Humble/Database/RepositoryTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace Humble\Database;
+
+use Humble\Query\QueryInterface;
+
+/**
+ * Trait RepositoryTrait
+ *
+ * @package Humble\Database
+ */
+trait RepositoryTrait
+{
+    /**
+     * @param string $query
+     * @param array  $params
+     * @param mixed  ...$criteria
+     */
+    public function buildCriteria(&$query, &$params, $criteria)
+    {
+        /** @var QueryInterface $item */
+        foreach ($criteria as $item) {
+            /** Getting an array which contains compiled query part and array of params */
+            $compiled = $item->getCompiled();
+
+            /** Adding query part to query string */
+            $query .= $compiled['query_part'];
+
+            /** Inserting params from criteria to general params array */
+            $params = array_merge($params, $compiled['params']);
+        }
+    }
+}
